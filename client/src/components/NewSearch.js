6ac35a4f26ac072a9ae048d83796343b9ad26828
Constantine Chutis <31866324+cchutis@users.noreply.github.com>
2020-05-20 18:06:59 -0400

    Added Build options

--- client/src/components/NewSearch.js
+++ client/src/components/NewSearch.js
@@ -7,8 +7,8 @@ import Places from './Places';
 export default function NewSearch(props) {
 
     const searchClient = algoliasearch(
-      process.env.REACT_APP_ALGOLIA_APP_ID,
-      process.env.REACT_APP_ALGOLIA_API_KEY
+     `${process.env.REACT_APP_ALGOLIA_APP_ID}`,
+      `${process.env.REACT_APP_ALGOLIA_API_KEY}`
     );
     return (
       <div>
