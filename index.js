7b5ce1d88953873f88dd6f684e64f60e0d82db51
Constantine Chutis <31866324+cchutis@users.noreply.github.com>
2020-05-23 16:36:37 -0400

    Fixed GET issues on refresh

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ app.get('/estimates', function(req, res) {
     })
 });
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname+'/client/build/index.html'), function (err) {
         if(err) {
             res.status(500).send(err)
