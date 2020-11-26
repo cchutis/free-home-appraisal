fa9625126def60ff13873dab135a3a699053e6c8
Constantine Chutis <31866324+cchutis@users.noreply.github.com>
2020-11-26 05:07:50 -0500

    Styled PDF Download button

--- client/src/components/PDFButton.js
+++ client/src/components/PDFButton.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import {SaveIcon} from '@material-ui/icons/Save';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        margin: theme.spacing(1),
+    },
+}));
 
 export default function PDFButton(props) {
+    const classes = useStyles();
     return (
-        <div>
-            <button onClick={props.savePage}>Download PDF</button>
+        <div className="download-section">
+            <Button 
+                onClick={props.savePage}
+                variant="contained"
+                color="primary"
+                size="large"
+                className={classes.button}
+                startIcon={<SaveIcon />}
+            >
+                Download PDF
+            </Button>
         </div>
     )
 }
