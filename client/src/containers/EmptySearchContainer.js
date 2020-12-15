7ae71b38a48dec9c05d3e5f6144a6285bc49e810
Constantine Chutis <31866324+cchutis@users.noreply.github.com>
2020-12-15 15:54:36 -0500

    Made it so the loading screen shows everytime a new search is conducted, instead of just the first time

--- client/src/containers/EmptySearchContainer.js
+++ client/src/containers/EmptySearchContainer.js
@@ -1,16 +1,10 @@
 import React from 'react';
-// import { CircularProgress } from '@material-ui/core';
-import LoadingIcon from '../assets/img/loading-icon.png';
 
 export default function EmptySearchContainer(props) {
     return (
         <div className="search-results">
             {props.isLoading ? 
-            <div className="loading-screen">
-                {/* <CircularProgress /> */}
-                <img src={LoadingIcon} alt="" />
-                <h1>The Accupraisal algorithm is finding your home's value!</h1>
-            </div>  :
+            null  :
             <img src="./img/howitworks-FHA.png" alt="" />
             }
         </div>
